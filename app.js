2bcee435e7fbe10f05d918a72693358b696d8d73
Ing. Luis Araujo <luiseduardoab97@gmail.com>
2020-08-20 22:11:24 -0300

    Mercadopago

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+const mpControllers = require('./controllers/mercadopago');
+const bodyParser = require('body-parser');
+
 var app = express();
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({'extended':'false'}));            
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -14,6 +25,15 @@ app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
 
+//Mercadopago
+app.get('/mp/notification', (req,res)=>{
+    mpControllers.getNotification(req,res);
+});
+app.post('/mp/payment', (req,res)=>{
+    mpControllers.createPayment(req,res);
+});
+
+//
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
---
+++ controllers/mercadopago.js
@@ -0,0 +1,86 @@
+const mercadopago = require('mercadopago');
+mercadopago.configure({
+    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+    sandbox: true
+})
+
+class MercadopagoController{
+
+    preferences;
+    urlFrontend;
+    urlBackend;
+
+    constructor(){
+        
+        this.urlBackend = "https://luisedo97-mp-commerce-nodejs.herokuapp.com"
+        this.urlFrontend = "https://luisedo97-mp-commerce-nodejs.herokuapp.com"
+
+        this.preferences = {
+            collector_id: 469485398,
+            //URLS
+            back_urls: {
+                success: this.urlFrontend+"/payment/success",
+                pending: this.urlFrontend+"/payment/pending",
+                failure: this.urlFrontend+"/payment/failure"
+            },
+            notification_url: this.urlBackend+"/mp/notification",
+            payment_methods:{
+                excluded_payment_methods: [
+                    {
+                        id: 'amex'
+                    }
+                ],
+                excluded_payment_types: [
+                    {
+                        id: 'atm'
+                    }
+                ],
+                installments: 6
+            },
+            payer:{
+                name: 'Lalo',
+                surname: 'Landa',
+                email: 'test_user_63274575@testuser.com',
+                address:{
+                    zip_code: '1111',
+                    street_name: 'False',
+                    street_number: 123
+                }
+            },
+            external_reference: 'luisaraujo@metodity.com',
+            items: []
+        }
+    }
+
+    async getNotification(req,res){
+        console.log(req,res)
+    }
+
+    async createPayment(req,res){
+        try {
+            
+            const preferences = {...this.preferences};
+
+            preferences.items.push({
+                id: 1234,
+                title: req.body.title,
+                description: "Dispositivo móvil de Tienda e-commerce",
+                picture_url: this.urlFrontend + req.body.img,
+                quantity: 1,
+                unit_price: parseInt(req.body.price)
+            });
+
+            const resMP = await mercadopago.preferences.create(this.preferences);
+            
+            //console.log(resMP);
+            res.status(200).send({init_point: resMP.body.init_point});
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+}
+
+module.exports = new MercadopagoController();
